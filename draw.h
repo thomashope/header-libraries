0d906d1560b1e22323c65b1ae783fb0b1e0ebee8
Thomas Hope <contact@thomashope.co.uk>
2017-02-06 00:54:29 +0000

    cleanup comments

--- draw.h
+++ draw.h
@@ -1,176 +1,209 @@
+////// README //////////////////////////////////////////////////////////////////
+//
+// DEPENDANCIES:
+//
+//  Programable pipline OpenGL. You can load all your OpenGL functions a library
+//  like glew.h.
+//
+// INSTALLATION:
+//
+//  1) #define TJH_DRAW_IMPLEMENTATION then #include this file in *ONE*
+//  .cpp file in your project.
+//
+//  2) Call `Draw::init()` somewhere after you have setup your OpenGL context.
+//  Beware this library will not setup an OpenGL context for you! You should
+//  create one yourself, or use my handy TJH_WINDOW library if you like :)
+//  
+//  3) Call `Draw::shutdown()` somewhere before you have destroyed your OpenGL
+//  context.
+//
+//  4) #include this file in all the files that want to draw stuff and call the
+//  functions!
+//
 
-// Get someone else to create the gl context
+////// LIBRARY OPTIONS /////////////////////////////////////////////////////////
+//
+// You can comment or uncomment the following #defines to enable or disable
+// additional library features.
+//
+// Change this to customise the namespace for this library
+#define TJH_DRAW_NAMESPACE Draw
 
-namespace Draw
+////// HEADER //////////////////////////////////////////////////////////////////
+
+namespace TJH_DRAW_NAMESPACE
 {
-	////// BASIC FUNCTIONS ////////////////////////////////////////////////////
-	
-	void init();		// TODO: return and error flag or something?
-	void shutdown();
-	void begin();
-	void end();
+    // BASIC FUNCTIONS /////////////////////////////////////////////////////
+    
+    void init();        // TODO: return and error flag or something?
+    void shutdown();
+    void begin();
+    void end();
 
-	////// SET STATE //////////////////////////////////////////////////////////
+    // SET STATE ///////////////////////////////////////////////////////////
 
-	// TODO: void setResolution( int width, int height );
-	//		This would scale the 
-	void setColor(GLfloat r, GLfloat g, GLfloat b, GLfloat a = 1.0f);
+    // TODO: void setResolution( int width, int height );
+    //      This would scale the 
+    void setColor( GLfloat r, GLfloat g, GLfloat b, GLfloat a = 1.0f );
 
-	////// PRIMATIVES /////////////////////////////////////////////////////////
+    // PRIMATIVES //////////////////////////////////////////////////////////
 
-	void line( GLfloat x1, GLfloat y1, GLfloat x2, GLfloat y2 );
-	void quad( GLfloat x1, GLfloat y1, GLfloat x2, GLfloat y2 );
-	void point( GLfloat x, GLfloat y );
+    void line( GLfloat x1, GLfloat y1, GLfloat x2, GLfloat y2 );
+    void circle( GLfloat x, GLfloat y, GLfloat radius );
+    void quad( GLfloat x1, GLfloat y1, GLfloat x2, GLfloat y2 );
+    void point( GLfloat x, GLfloat y );
 }
 
+////// IMPLEMENTATION //////////////////////////////////////////////////////////
 #ifdef TJH_DRAW_IMPLEMENTATION
-	
-namespace Draw
+    
+namespace TJH_DRAW_NAMESPACE
 {
-	GLuint vao_ = 0;
-	GLuint vbo_ = 0;
-	GLuint shader_program_ = 0;
-	GLint colour_uniform_ = -1;
+    // 'PRIVATE' MEMBERS
+    GLuint vao_ = 0;
+    GLuint vbo_ = 0;
+    GLuint shader_program_ = 0;
+    GLint colour_uniform_ = -1;
 
-	void init()
-	{
-		const GLchar* vert_src =
-		    "#version 150 core\n"
-		    "in vec2 vPos;"
-		    "void main()"
-		    "{"
-		    "    gl_Position = vec4(vPos, 0.0, 1.0);"
-		    "}";
-		const GLchar* frag_src =
-			"#version 150 core\n"
-			"uniform vec4 colour;"
-			"out vec4 outColour;"
-			"void main()"
-			"{"
-			"    outColour = colour;"
-			"}";
-		GLuint vertex_shader_ = glCreateShader(GL_VERTEX_SHADER);
-		GLuint fragment_shader_ = glCreateShader(GL_FRAGMENT_SHADER);
-		if( vertex_shader_ == 0 || fragment_shader_ == 0 ) printf("ERROR: cold not init shaders!");
+    void init()
+    {
+        const GLchar* vert_src =
+            "#version 150 core\n"
+            "in vec2 vPos;"
+            "void main()"
+            "{"
+            "    gl_Position = vec4(vPos, 0.0, 1.0);"
+            "}";
+        const GLchar* frag_src =
+            "#version 150 core\n"
+            "uniform vec4 colour;"
+            "out vec4 outColour;"
+            "void main()"
+            "{"
+            "    outColour = colour;"
+            "}";
+        GLuint vertex_shader_ = glCreateShader(GL_VERTEX_SHADER);
+        GLuint fragment_shader_ = glCreateShader(GL_FRAGMENT_SHADER);
+        if( vertex_shader_ == 0 || fragment_shader_ == 0 ) printf("ERROR: cold not init shaders!");
 
-		// Compile and check the vertex shader
-		glShaderSource( vertex_shader_, 1, &vert_src, NULL );
-		glCompileShader( vertex_shader_ );
-		GLint status = GL_TRUE;
-		glGetShaderiv( vertex_shader_, GL_COMPILE_STATUS, &status );
-		if( status != GL_TRUE )
-		{
-			printf("ERROR: could not compile vertex shader\n");
-	        // Get the length of the error log
-	        GLint log_length = 0;
-	        glGetShaderiv(vertex_shader_, GL_INFO_LOG_LENGTH, &log_length);
+        // Compile and check the vertex shader
+        glShaderSource( vertex_shader_, 1, &vert_src, NULL );
+        glCompileShader( vertex_shader_ );
+        GLint status = GL_TRUE;
+        glGetShaderiv( vertex_shader_, GL_COMPILE_STATUS, &status );
+        if( status != GL_TRUE )
+        {
+            printf("ERROR: could not compile vertex shader\n");
+            // Get the length of the error log
+            GLint log_length = 0;
+            glGetShaderiv(vertex_shader_, GL_INFO_LOG_LENGTH, &log_length);
 
-	        // Now get the error log itself
-	        GLchar buffer[log_length];
-	        glGetShaderInfoLog( vertex_shader_, log_length, NULL, buffer );
-			printf("%s\n", buffer);
-		}
+            // Now get the error log itself
+            GLchar buffer[log_length];
+            glGetShaderInfoLog( vertex_shader_, log_length, NULL, buffer );
+            printf("%s\n", buffer);
+        }
 
-		// Compile and check the fragment shader
-		glShaderSource( fragment_shader_, 1, &frag_src, NULL );
-		glCompileShader( fragment_shader_ );
-		status = GL_TRUE;
-		glGetShaderiv( fragment_shader_, GL_COMPILE_STATUS, &status );
-		if( status != GL_TRUE )
-		{
-			printf("ERROR: could not compile fragment shader\n");
-	        // Get the length of the error log
-	        GLint log_length = 0;
-	        glGetShaderiv(fragment_shader_, GL_INFO_LOG_LENGTH, &log_length);
+        // Compile and check the fragment shader
+        glShaderSource( fragment_shader_, 1, &frag_src, NULL );
+        glCompileShader( fragment_shader_ );
+        status = GL_TRUE;
+        glGetShaderiv( fragment_shader_, GL_COMPILE_STATUS, &status );
+        if( status != GL_TRUE )
+        {
+            printf("ERROR: could not compile fragment shader\n");
+            // Get the length of the error log
+            GLint log_length = 0;
+            glGetShaderiv(fragment_shader_, GL_INFO_LOG_LENGTH, &log_length);
 
-	        // Now get the error log itself
-	        GLchar buffer[log_length];
-	        glGetShaderInfoLog( fragment_shader_, log_length, NULL, buffer );
-			printf("%s\n", buffer);
-		}
+            // Now get the error log itself
+            GLchar buffer[log_length];
+            glGetShaderInfoLog( fragment_shader_, log_length, NULL, buffer );
+            printf("%s\n", buffer);
+        }
 
-		shader_program_ = glCreateProgram();
-		glAttachShader( shader_program_, vertex_shader_ );
-		glAttachShader( shader_program_, fragment_shader_ );
-		glBindFragDataLocation( shader_program_, 0, "outColour");
-		glLinkProgram( shader_program_ );
-		glUseProgram( shader_program_ );
-		glDeleteShader(vertex_shader_);
-		glDeleteShader(fragment_shader_);
+        shader_program_ = glCreateProgram();
+        glAttachShader( shader_program_, vertex_shader_ );
+        glAttachShader( shader_program_, fragment_shader_ );
+        glBindFragDataLocation( shader_program_, 0, "outColour");
+        glLinkProgram( shader_program_ );
+        glUseProgram( shader_program_ );
+        glDeleteShader(vertex_shader_);
+        glDeleteShader(fragment_shader_);
 
-		colour_uniform_ = glGetUniformLocation(shader_program_, "colour");
+        colour_uniform_ = glGetUniformLocation(shader_program_, "colour");
 
-		glGenVertexArrays(1, &vao_);
-		glBindVertexArray(vao_);
-		glGenBuffers(1, &vbo_);
-		glBindBuffer(GL_ARRAY_BUFFER, vbo_);
+        glGenVertexArrays(1, &vao_);
+        glBindVertexArray(vao_);
+        glGenBuffers(1, &vbo_);
+        glBindBuffer(GL_ARRAY_BUFFER, vbo_);
 
-		GLint posAtrib = glGetAttribLocation(shader_program_, "vPos");
-		if( posAtrib == -1 ) printf("ERROR: position attribute not found in shader\n");
-		glEnableVertexAttribArray(posAtrib);
-		glVertexAttribPointer(posAtrib, 2, GL_FLOAT, GL_FALSE, 0, 0);
+        GLint posAtrib = glGetAttribLocation(shader_program_, "vPos");
+        if( posAtrib == -1 ) printf("ERROR: position attribute not found in shader\n");
+        glEnableVertexAttribArray(posAtrib);
+        glVertexAttribPointer(posAtrib, 2, GL_FLOAT, GL_FALSE, 0, 0);
 
-		glBindVertexArray(0);
-		glBindBuffer(GL_ARRAY_BUFFER, 0);
-		glUseProgram(0);
-	}
-	void shutdown()
-	{
-		glDeleteProgram(shader_program_);
-	}
-	void begin()
-	{
-		// store previous values and set stuff, see dear imgui
-		glUseProgram(shader_program_);
-	}
-	void end()
-	{
-		// TOOD: This should restore the previous state instead
-		glUseProgram(0);
-		glBindVertexArray(0);
-		glBindBuffer(GL_ARRAY_BUFFER, 0);
-	}
+        glBindVertexArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        glUseProgram(0);
+    }
+    void shutdown()
+    {
+        glDeleteProgram(shader_program_);
+    }
+    void begin()
+    {
+        // store previous values and set stuff, see dear imgui
+        glUseProgram(shader_program_);
+    }
+    void end()
+    {
+        // TOOD: This should restore the previous state instead
+        glUseProgram(0);
+        glBindVertexArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+    }
 
-	void setColor(GLfloat r, GLfloat g, GLfloat b, GLfloat a )
-	{
-		glUniform4f( colour_uniform_, r, g, b, a );
-	}
+    void setColor(GLfloat r, GLfloat g, GLfloat b, GLfloat a )
+    {
+        glUniform4f( colour_uniform_, r, g, b, a );
+    }
 
-	void line( GLfloat x1, GLfloat y1, GLfloat x2, GLfloat y2 )
-	{
-		glBindVertexArray(vao_);
-		glBindBuffer(GL_ARRAY_BUFFER, vbo_);
+    void line( GLfloat x1, GLfloat y1, GLfloat x2, GLfloat y2 )
+    {
+        glBindVertexArray(vao_);
+        glBindBuffer(GL_ARRAY_BUFFER, vbo_);
 
-		GLfloat verts[] = {
-			x1, y1, x2, y2
-		};
+        GLfloat verts[] = {
+            x1, y1, x2, y2
+        };
 
-		glBufferData(GL_ARRAY_BUFFER, sizeof(verts), verts, GL_STREAM_DRAW);
-		glDrawArrays(GL_LINES, 0, 2);
-	}
-	void quad( GLfloat x1, GLfloat y1, GLfloat x2, GLfloat y2 )
-	{
-		glBindVertexArray(vao_);
-		glBindBuffer(GL_ARRAY_BUFFER, vbo_);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(verts), verts, GL_STREAM_DRAW);
+        glDrawArrays(GL_LINES, 0, 2);
+    }
+    void quad( GLfloat x1, GLfloat y1, GLfloat x2, GLfloat y2 )
+    {
+        glBindVertexArray(vao_);
+        glBindBuffer(GL_ARRAY_BUFFER, vbo_);
 
-		GLfloat verts[] = {
-			x1, y1, x2, y1, x2, y2,
-			x1, y1, x2, y2, x1, y2
-		};
+        GLfloat verts[] = {
+            x1, y1, x2, y1, x2, y2,
+            x1, y1, x2, y2, x1, y2
+        };
 
-		glBufferData(GL_ARRAY_BUFFER, sizeof(verts), verts, GL_STREAM_DRAW);
-		glDrawArrays(GL_TRIANGLES, 0, 6);
-	}
-	void point( GLfloat x, GLfloat y )
-	{
-		glBindVertexArray(vao_);
-		glBindBuffer(GL_ARRAY_BUFFER, vbo_);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(verts), verts, GL_STREAM_DRAW);
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+    }
+    void point( GLfloat x, GLfloat y )
+    {
+        glBindVertexArray(vao_);
+        glBindBuffer(GL_ARRAY_BUFFER, vbo_);
 
-		GLfloat verts[] = { x, y };
+        GLfloat verts[] = { x, y };
 
-		glBufferData(GL_ARRAY_BUFFER, sizeof(verts), verts, GL_STREAM_DRAW);
-		glDrawArrays(GL_POINTS, 0, 2);
-	}
+        glBufferData(GL_ARRAY_BUFFER, sizeof(verts), verts, GL_STREAM_DRAW);
+        glDrawArrays(GL_POINTS, 0, 2);
+    }
 }
 
 #endif
--- window.h
+++ window.h
@@ -1,106 +1,103 @@
+////// README //////////////////////////////////////////////////////////////////
+//
 // Define TJH_WINDOW_IMPLEMENTATION in *ONE FILE ONLY*
 // This relies on GLEW and SDL2
 // To compile on mac, install SDL2 and glew using homebrew
 // then link during compilation with -lSDL2 -lglew -framework OpenGL
 
-////// LIBRARY OPTIONS ////////////////////////////////////////////////////////
+////// LIBRARY OPTIONS /////////////////////////////////////////////////////////
+//
+// You can comment or uncomment the following #defines to enable or disable
+// additional library features.
+//
+// If defined init() and shutdown() will setup and quit the SDL2 library for you
+#define TJH_WINDOW_AUTO_INIT_AND_SHUTDOWN_SDL
+// If defined init() will call glewInit() with glewExperimental set to true
+#define TJH_WINDOW_AUTO_INIT_GLEW
+// If defined this file will include SDL.h
+#define TJH_WINDOW_INCLUDE_SDL
+// If defined this file will include glew.h
+#define TJH_WINDOW_INCLUDE_GLEW
+// Change this to customise the namespace for this library
+#define TJH_WINDOW_NAMESPACE Window
 
-// Set these to the location of your libray headers!
+// IMPORTANT: If any of the above options are set these must also be set correctly
 #define TJH_WINDOW_SDL_H_LOCATION <SDL2/SDL.h>
 #define TJH_WINDOW_GLEW_H_LOCATION <GL/glew.h>
 
-// Options for your convinience!
-#define TJH_WINDOW_AUTO_INIT_AND_SHUTDOWN_SDL
-#define TJH_WINDOW_AUTO_INIT_GLEW
-#define TJH_WINDOW_INCLUDE_SDL
-#define TJH_WINDOW_INCLUDE_GLEW
+////// HEADER //////////////////////////////////////////////////////////////////
 
-// Optional header includes
 #ifdef TJH_WINDOW_INCLUDE_SDL
-	#include TJH_WINDOW_SDL_H_LOCATION
+    #include TJH_WINDOW_SDL_H_LOCATION
 #endif
 #ifdef TJH_WINDOW_INCLUDE_GLEW
-	#include TJH_WINDOW_GLEW_H_LOCATION
+    #include TJH_WINDOW_GLEW_H_LOCATION
 #endif
 
-
-////// HEADER /////////////////////////////////////////////////////////////////
-namespace Window
+namespace TJH_WINDOW_NAMESPACE
 {
-	// TODO: return an error flag or something?
-	void init(const char* title, int width, int height);
-	void shutdown();
-	void present();
-	void getSize( int* width, int* height );
-	SDL_Window* getSDLWindow();
-	SDL_GLContext getSDLGLContext();
+    // TODO: init() return an error flag or something?
+    void init(const char* title, int width, int height);    // Call one, can init SDL and GLEW if macros are set
+    void shutdown();                                        // Shuts down SDL window, and library if asked
+    void present();                                         // Flip the buffers
+    void getSize( int* width, int* height );                // Get the current size of the window
 
-	// PUBLIC MEMBERS
-	extern SDL_Window* sdl_window_;
-	extern SDL_GLContext sdl_gl_context_;
+    // PUBLIC MEMBERS
+    extern SDL_Window* sdl_window;
+    extern SDL_GLContext sdl_gl_context;
 }
 
-////// IMPLEMENTATION /////////////////////////////////////////////////////////
+////// IMPLEMENTATION //////////////////////////////////////////////////////////
 #ifdef TJH_WINDOW_IMPLEMENTATION
 
-namespace Window
+namespace TJH_WINDOW_NAMESPACE
 {
-	void init(const char* title, int width, int height)
-	{
-	#ifdef TJH_WINDOW_AUTO_INIT_AND_SHUTDOWN_SDL
-		SDL_Init(SDL_INIT_EVERYTHING);
-	#endif // TJH_WINDOW_AUTO_INIT_AND_SHUTDOWN_SDL
+    // PUBLIC MEMBERS
+    SDL_Window* sdl_window = NULL;
+    SDL_GLContext sdl_gl_context = NULL;
 
-	    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-	    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
-	    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
+    void init(const char* title, int width, int height)
+    {
+    #ifdef TJH_WINDOW_AUTO_INIT_AND_SHUTDOWN_SDL
+        SDL_Init(SDL_INIT_EVERYTHING);
+    #endif
 
-		sdl_window_ = SDL_CreateWindow( title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, SDL_WINDOW_OPENGL );
-		sdl_gl_context_ = SDL_GL_CreateContext( sdl_window_ );
+        SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+        SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+        SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
 
-	#ifdef TJH_WINDOW_AUTO_INIT_GLEW
-		glewExperimental = GL_TRUE;
-		GLenum error = glewInit();
-		if( error != GLEW_OK ) printf("ERROR: starting glew %d", error);
-	#endif // TJH_WINDOW_AUTO_INIT_GLEW
-	}
+        sdl_window = SDL_CreateWindow( title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, SDL_WINDOW_OPENGL );
+        sdl_gl_context = SDL_GL_CreateContext( sdl_window );
 
-	void shutdown()
-	{
-		SDL_GL_DeleteContext( sdl_gl_context_ );
-		sdl_gl_context_ = NULL;
+    #ifdef TJH_WINDOW_AUTO_INIT_GLEW
+        glewExperimental = GL_TRUE;
+        GLenum error = glewInit();
+        if( error != GLEW_OK ) printf("ERROR: starting glew %d", error);
+    #endif
+    }
 
-		SDL_DestroyWindow( sdl_window_ );
-		sdl_window_ = NULL;
+    void shutdown()
+    {
+        SDL_GL_DeleteContext( sdl_gl_context );
+        sdl_gl_context = NULL;
 
-	#ifdef TJH_WINDOW_AUTO_INIT_AND_SHUTDOWN_SDL
-		SDL_Quit();
-	#endif // TJH_WINDOW_AUTO_INIT_AND_SHUTDOWN_SDL
-	}
+        SDL_DestroyWindow( sdl_window );
+        sdl_window = NULL;
 
-	void present()
-	{
-		SDL_GL_SwapWindow( sdl_window_ );
-	}
+    #ifdef TJH_WINDOW_AUTO_INIT_AND_SHUTDOWN_SDL
+        SDL_Quit();
+    #endif
+    }
 
-	void getSize( int* width, int* height )
-	{
-		SDL_GetWindowSize( sdl_window_, width, height );
-	}
+    void present()
+    {
+        SDL_GL_SwapWindow( sdl_window );
+    }
 
-	SDL_Window* getSDLWindow()
-	{
-		return sdl_window_;
-	}
-
-	SDL_GLContext getSDLGLContext()
-	{
-		return sdl_gl_context_;
-	}
-
-	// MEMBERS
-	SDL_Window* sdl_window_ = NULL;
-	SDL_GLContext sdl_gl_context_ = NULL;
+    void getSize( int* width, int* height )
+    {
+        SDL_GetWindowSize( sdl_window, width, height );
+    }
 }
 
-#endif // TJH_WINDOW_IMPLEMENTATION
+#endif // END TJH_WINDOW_IMPLEMENTATION
